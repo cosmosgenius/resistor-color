83ea0ecfee5fae9b193afe85f1e3ebc674d4a364
Sharat M R <cosmosgenius@gmail.com>
2013-09-09 23:17:29 +0530

    add commits and created a preprocessing function

--- resistor.js
+++ resistor.js
@@ -44,6 +44,29 @@
         return '-1';
     }
 
+    function toStandardForm(value){
+        var replace_char;
+        //remove any zeros present in the start of string
+        value = String(value).replace(/^0*/,'');
+
+        //find the replace character if it exist else default to 'R'
+        //and add R to the last of the value so as to make is consistent
+        //if char is not present
+        replace_char = value.match(/[mMkKrR]/);
+        if(!replace_char){
+            replace_char = ['R'];
+            value = value + 'R';
+        }
+
+        //replace '.' is exist with replace_char
+        if(value.indexOf(".")!== -1){
+            value = value.replace(replace_char[0], '');
+            value = value.replace(".",replace_char[0]);
+        }
+
+        return value;
+    }
+
     /**
      * Return the color bands
      * @param  {String}     resistor_value resistor value
@@ -51,7 +74,7 @@
      * @param  {interger}   mul      multiplier([0,3,6])
      * @return {Dict object}         band color
      */
-    exports.resistorToColor = function (resistor_value, tol, mul){
+    exports.resistorToColor = function (resistor_value, tol){
         var digit = [0, 0, 0],
             digits = [],
             dec = -1,
@@ -59,14 +82,26 @@
                 value:'',
                 pos:''
             },
-            multiplier;
+            multiplier,
+            significant_digit;
+
+        //defaulting parameters if not present
+        tol = tol || 5;
+        mul = mul || null;
+
+        //calculating number of digit to consider before taking the digit for multiplier
+        significant_digit = tol > 2 ? 2 : 3;
+
+        //check if resistor value exist or not...    
         if (!resistor_value){
             throw ("resistor: invalid parameter");
         }
-        resistor_value = String(resistor_value).replace(/^0*/,'');
-        tol = tol || 5;
-        mul = mul || null;
-        var significant_digit = tol > 2 ? 2 : 3;
+        
+        //preprocess the value
+        resistor_value = toStandardForm(resistor_value);
+
+        //setting the multiplier value k=1000*100, m=100000*100, and default=100
+        //extra 100 is used to get accuracy when doing float division and multiplication
         if(/[kK]/.test(resistor_value)){
             mul['value'] = 100000;
         }else if(/[mM]/.test(resistor_value)){
@@ -74,12 +109,16 @@
         }else{
             mul['value'] = 100;
         }
-        if(/[.]/.test(resistor_value)){
-            resistor_value = resistor_value.replace(/[rRkKmM]/,'');
-        }else{
-            resistor_value = resistor_value.replace(/[rRkKmM]/,'.');
-        }
+
+        //remove multiplier character and replace by . if not present.
+        resistor_value = resistor_value.replace(/[rRkKmM]/,'.');
+
+        //convert resistor_value to float by unioperator +
+        //round of to precision 4 to remove float point bug eg try (1.16 * 100)
+        //remove trailing zeros
+        //finally convert to string by ""+
         resistor_value = (""+ +(+resistor_value * mul['value']).toFixed(4));
+
         for (var i = 0 ; i< significant_digit ; i++){
             digits.push(resistor_value[i]?resistor_value[i]:0);
         }
